a25f0ecd677dc53922af513f69be748f26acacd4
James Messinger <james.messinger@gmail.com>
2020-04-17 14:04:54 -0500

    Refactored Package and Shipment to use multiple inheritance instead of duplicated code

--- src/package.ts
+++ src/package.ts
@@ -7,128 +7,138 @@ import { Dimensions } from "./dimensions";
 import { Currency, MonetaryValue } from "./monetary-value";
 import { PackageItem } from "./package-item";
 import { Packaging } from "./shipping-provider";
-import { Identifier } from "./types";
+import { Constructor, Identifier } from "./types";
 import { Weight } from "./weight";
 
 /**
  * Identifies a package
  */
-export class PackageIdentifier {
-  /**
-   * The carrier tracking number
-   */
-  public readonly trackingNumber: string;
-
-  /**
-   * Alternative identifiers associated with this package
-   */
-  public readonly identifiers: ReadonlyArray<Identifier>;
-
+export class PackageIdentifier extends packageIdentifierMixin() {
   public constructor(config: PackageIdentifierConfig) {
-    assert.type.object(config, "package");
-    this.trackingNumber = assert.string.nonWhitespace(config.trackingNumber, "tracking number");
-    this.identifiers = assert.array.ofIdentifiers(config.identifiers, "package identifiers", []);
-
-    // Prevent modifications after validation
-    Object.freeze(this);
-    Object.freeze(this.identifiers);
+    super(config, false);
   }
 }
 
 /**
  * A package that has not yet been created and has no identifiers yet
  */
-export class NewPackage {
-  /**
-   * The packaging used
-   */
-  public readonly packaging: Packaging;
-
-  /**
-   * The package dimensions
-   */
-  public readonly dimensions?: Dimensions;
-
-  /**
-   * The package weight
-   */
-  public readonly weight?: Weight;
-
-  /**
-   * The insured value of this package
-   */
-  public readonly insuredValue: MonetaryValue;
-
-  /**
-   * Indicates whether the package contains alcohol
-   */
-  public readonly containsAlcohol: boolean;
-
-  /**
-   * Indicates whether the package cannot be processed automatically due to size, shape, weight, etc.
-   * and requires manual handling.
-   */
-  public readonly isNonMachineable: boolean;
-
-  /**
-   * Customized strings the end user expects to appear on their label.
-   * The exact location on the label depends on the carrier. Some carriers
-   * may limit the number of allowed label messages, or not support them at all.
-   */
-  public readonly labelMessages: ReadonlyArray<string>;
-
-  /**
-   * Describes the items inside the package
-   */
-  public readonly contents: ReadonlyArray<PackageItem>;
-
-  public constructor(app: App, config: NewPackageConfig, freeze = true) {
-    assert.type.object(config, "package");
-    this.packaging = app._references.lookup(config.packagingID, Packaging, "packaging");
-    this.dimensions = (config.dimensions || this.packaging.requiresDimensions)
-      ? new Dimensions(config.dimensions!) : undefined;
-    this.weight = (config.weight || this.packaging.requiresWeight)
-      ? new Weight(config.weight!) : undefined;
-    this.insuredValue = new MonetaryValue(config.insuredValue || { value: 0, currency: Currency.UnitedStatesDollar });
-    this.containsAlcohol = assert.type.boolean(config.containsAlcohol, "containsAlcohol flag", false);
-    this.isNonMachineable = assert.type.boolean(config.isNonMachineable, "isNonMachineable flag", false);
-    this.labelMessages = assert.array(config.labelMessages, "label messages", [])
-      .map((message) => assert.string(message, "label message"));
-    this.contents = assert.array(config.contents, "package contents", [])
-      .map((item: PackageItemConfig) => new PackageItem(item));
-
-    if (freeze) {
-      // Prevent modifications after validation
-      Object.freeze(this);
-      Object.freeze(this.labelMessages);
-      Object.freeze(this.contents);
-    }
+export class NewPackage extends newPackageMixin() {
+  public constructor(app: App, config: NewPackageConfig) {
+    super(app, config, false);
   }
 }
 
 /**
  * A package that has already been created and assigned identifiers
  */
-export class Package extends NewPackage {
-  /**
-   * The carrier tracking number
-   */
-  public readonly trackingNumber!: string;
+export interface Package extends PackageIdentifier, NewPackage {}
 
-  /**
-   * Alternative identifiers associated with this package
-   */
-  public readonly identifiers!: ReadonlyArray<Identifier>;
-
+/**
+ * A package that has already been created and assigned identifiers
+ */
+export class Package extends newPackageMixin(PackageIdentifier) {
   public constructor(app: App, config: PackageConfig) {
-    super(app, config, false);
-    this.trackingNumber = assert.string.nonWhitespace(config.trackingNumber, "tracking number");
-    this.identifiers = assert.array.ofIdentifiers(config.identifiers, "package identifiers", []);
+    super(app, config, true);
 
     // Prevent modifications after validation
     Object.freeze(this);
-    Object.freeze(this.identifiers);
-    Object.freeze(this.labelMessages);
-    Object.freeze(this.contents);
   }
+}
+
+
+function packageIdentifierMixin() {
+  return class PackageIdentifierMixin {
+    /**
+     * The carrier tracking number
+     */
+    public readonly trackingNumber: string;
+
+    /**
+     * Alternative identifiers associated with this package
+     */
+    public readonly identifiers: ReadonlyArray<Identifier>;
+
+    public constructor(config: PackageIdentifierConfig, isMixin: boolean) {
+      assert.type.object(config, "package");
+      this.trackingNumber = assert.string.nonWhitespace(config.trackingNumber, "tracking number");
+      this.identifiers = assert.array.ofIdentifiers(config.identifiers, "package identifiers", []);
+
+      // Prevent modifications after validation
+      Object.freeze(this.identifiers);
+
+      // Don't freeze the base object yet if this is a mixin
+      isMixin || Object.freeze(this);
+    }
+  };
+}
+
+function newPackageMixin(base: Constructor = Object) {
+  return class NewPackageMixin extends base {
+    /**
+     * The packaging used
+     */
+    public readonly packaging: Packaging;
+
+    /**
+     * The package dimensions
+     */
+    public readonly dimensions?: Dimensions;
+
+    /**
+     * The package weight
+     */
+    public readonly weight?: Weight;
+
+    /**
+     * The insured value of this package
+     */
+    public readonly insuredValue: MonetaryValue;
+
+    /**
+     * Indicates whether the package contains alcohol
+     */
+    public readonly containsAlcohol: boolean;
+
+    /**
+     * Indicates whether the package cannot be processed automatically due to size, shape, weight, etc.
+     * and requires manual handling.
+     */
+    public readonly isNonMachineable: boolean;
+
+    /**
+     * Customized strings the end user expects to appear on their label.
+     * The exact location on the label depends on the carrier. Some carriers
+     * may limit the number of allowed label messages, or not support them at all.
+     */
+    public readonly labelMessages: ReadonlyArray<string>;
+
+    /**
+     * Describes the items inside the package
+     */
+    public readonly contents: ReadonlyArray<PackageItem>;
+
+    public constructor(app: App, config: NewPackageConfig, isMixin: boolean) {
+      super(config, isMixin);
+      assert.type.object(config, "package");
+      this.packaging = app._references.lookup(config.packagingID, Packaging, "packaging");
+      this.dimensions = (config.dimensions || this.packaging.requiresDimensions)
+        ? new Dimensions(config.dimensions!) : undefined;
+      this.weight = (config.weight || this.packaging.requiresWeight)
+        ? new Weight(config.weight!) : undefined;
+      this.insuredValue = new MonetaryValue(config.insuredValue || { value: 0, currency: Currency.UnitedStatesDollar });
+      this.containsAlcohol = assert.type.boolean(config.containsAlcohol, "containsAlcohol flag", false);
+      this.isNonMachineable = assert.type.boolean(config.isNonMachineable, "isNonMachineable flag", false);
+      this.labelMessages = assert.array(config.labelMessages, "label messages", [])
+        .map((message) => assert.string(message, "label message"));
+      this.contents = assert.array(config.contents, "package contents", [])
+        .map((item: PackageItemConfig) => new PackageItem(item));
+
+      // Prevent modifications after validation
+      Object.freeze(this.labelMessages);
+      Object.freeze(this.contents);
+
+      // Don't freeze the base object yet if this is a mixin
+      isMixin || Object.freeze(this);
+    }
+  };
 }
--- src/shipment.ts
+++ src/shipment.ts
@@ -10,219 +10,229 @@ import { ErrorCode, IpaasError } from "./errors";
 import { Currency, MonetaryValue } from "./monetary-value";
 import { NewPackage, Package } from "./package";
 import { DeliveryConfirmation, DeliveryService } from "./shipping-provider";
-import { Identifier } from "./types";
+import { Constructor, Identifier } from "./types";
 
 /**
  * Identifies a shipment
  */
-export class ShipmentIdentifier {
-  /**
-   * The carrier tracking number
-   */
-  public readonly trackingNumber: string;
-
-  /**
-   * Alternative identifiers associated with this shipment
-   */
-  public readonly identifiers: ReadonlyArray<Identifier>;
-
+export class ShipmentIdentifier extends shipmentIdentifierMixin() {
   public constructor(config: ShipmentIdentifierConfig) {
-    assert.type.object(config, "shipment");
-    this.trackingNumber = assert.string.nonWhitespace(config.trackingNumber, "tracking number");
-    this.identifiers = assert.array.ofIdentifiers(config.identifiers, "shipment identifiers", []);
-
-    // Prevent modifications after validation
-    Object.freeze(this);
-    Object.freeze(this.identifiers);
+    super(config, false);
   }
 }
 
 /**
  * A shipment that has not yet been created and has no identifiers yet
  */
-export class NewShipment {
-  /**
-   * The delivery service to use
-   */
-  public readonly deliveryService: DeliveryService;
-
-  /**
-   * The ID of the requested delivery confirmation
-   */
-  public readonly deliveryConfirmation?: DeliveryConfirmation;
-
-  /**
-   * The sender's contact info and address
-   */
-  public readonly shipFrom: Address;
-
-  /**
-   * The recipient's contact info and address
-   */
-  public readonly shipTo: Address;
-
-  /**
-   * The return address. Defautls to the `shipFrom` address
-   */
-  public readonly returnTo: Address;
-
-  /**
-   * The date/time that the package is expected to ship.
-   * This is not guaranteed to be in the future.
-   */
-  public readonly shipDateTime: Date;
-
-  /**
-   * Indicates how a non-deliverable package should be handled
-   */
-  public readonly nonDeliveryAction: NonDeliveryAction;
-
-  /**
-   * Which party will be insuring the shipment
-   */
-  public readonly insuranceProvider: InsuranceProvider;
-
-  /**
-   * The total insured value of all packages in the shipment
-   */
-  public readonly totalInsuredValue: MonetaryValue;
-
-  /**
-   * The original (outgoing) shipment that this return shipment is for.
-   * This associates the two shipments, which is required by some carriers.
-   * This field is `undefined` if this is not a return shipment, or if no
-   * outbound shipment was specified.
-   */
-  public readonly outboundShipment?: ShipmentIdentifier;
-
-  /**
-   * Indicates whether this is a return shipment
-   */
-  public readonly isReturn: boolean;
-
-  /**
-   * Indicates whether the shipment cannot be processed automatically due to size, shape, weight, etc.
-   * and requires manual handling.
-   *
-   * This property is `true` if any package in the shipment is non-machineable.
-   */
-  public get isNonMachineable(): boolean {
-    return this.packages.some((pkg) => pkg.isNonMachineable);
-  }
-
-  /**
-   * Billing details.
-   */
-  public billing: {
-    /**
-     * Indicates who customs duties are billed to. Defaults to the sender
-     */
-    dutiesPaidBy: BilledParty;
-
-    /**
-     * Indicates who delivery charges are billed to. Defaults to the sender
-     */
-    deliveryPaidBy: BilledParty;
-
-    /**
-     * The account number of the third-party that is responsible for shipping costs
-     */
-    account?: string;
-
-    /**
-     * The postal code of the third-party that is responsible for shipping costs
-     */
-    postalCode?: string;
-
-    /**
-     * The country of the third-party that is responsible for shipping costs
-     */
-    country?: Country;
-  };
-
-  /**
-   * The list of packages in the shipment
-   */
-  public readonly packages: ReadonlyArray<NewPackage>;
-
-  public constructor(app: App, config: NewShipmentConfig, freeze = true) {
-    assert.type.object(config, "shipment");
-    this.deliveryService = app._references.lookup(config.deliveryServiceID, DeliveryService, "delivery service");
-    this.deliveryConfirmation = app._references.get(config.deliveryConfirmationID, DeliveryConfirmation, "delivery confirmation");
-    this.shipFrom = new Address(config.shipFrom);
-    this.shipTo = new Address(config.shipTo);
-    this.returnTo = config.returnTo ? new Address(config.returnTo) : new Address(config.shipFrom);
-    this.shipDateTime = assert.type.date(config.shipDateTime, "shipment date/time");
-    this.nonDeliveryAction = assert.string.enum(config.nonDeliveryAction, NonDeliveryAction, "non-delivery action");
-    this.insuranceProvider = assert.string.enum(
-        config.insuranceProvider, InsuranceProvider, "insurance provider", InsuranceProvider.Carrier);
-    this.outboundShipment = config.outboundShipment && new ShipmentIdentifier(config.outboundShipment);
-    this.isReturn = assert.type.boolean(config.isReturn, "isReturn flag", false);
-
-    // If there is no billing info, then the sender is billed by default.
-    // If billing a third-party, then account, postalCode, and country are required.
-    let billing = assert.type.object(config.billing, "billing info", {});
-    let isThirdParty =
-      (billing.dutiesPaidBy === BilledParty.ThirdParty) || (billing.deliveryPaidBy === BilledParty.ThirdParty);
-
-    this.billing = {
-      dutiesPaidBy: assert.string.enum(billing.dutiesPaidBy, BilledParty, "dutiesPaidBy", BilledParty.Sender),
-      deliveryPaidBy: assert.string.enum(billing.deliveryPaidBy, BilledParty, "deliveryPaidBy", BilledParty.Sender),
-      account: billing.account || isThirdParty
-        ? assert.string.nonWhitespace(billing.account, "billing account") : undefined,
-      postalCode: billing.postalCode || isThirdParty
-        ? assert.string.nonWhitespace(billing.postalCode, "billing postal code") : undefined,
-      country: billing.country || isThirdParty
-        ? assert.string.enum(billing.country, Country, "billing country") : undefined,
-    };
-
-    this.packages = assert.array.nonEmpty(config.packages, "packages")
-      .map((parcel: PackageConfig) => new NewPackage(app, parcel));
-
-    // NOTE: This must be calculated AFTER setting the packages property
-    this.totalInsuredValue = calculateTotalInsuranceAmount(this.packages);
-
-    if (freeze) {
-      // Prevent modifications after validation
-      Object.freeze(this);
-      Object.freeze(this.billing);
-      Object.freeze(this.packages);
-    }
+export class NewShipment extends newShipmentMixin() {
+  public constructor(app: App, config: NewShipmentConfig) {
+    super(app, config, false);
   }
 }
 
 /**
  * A shipment that has already been created and assigned identifiers
  */
-export class Shipment extends NewShipment {
-  /**
-   * The carrier tracking number
-   */
-  public readonly trackingNumber: string;
+export interface Shipment extends ShipmentIdentifier, NewShipment {}
 
-  /**
-   * Alternative identifiers associated with this shipment
-   */
-  public readonly identifiers: ReadonlyArray<Identifier>;
-
+/**
+ * A shipment that has already been created and assigned identifiers
+ */
+export class Shipment extends newShipmentMixin(ShipmentIdentifier) {
   /**
    * The list of packages in the shipment
    */
   public readonly packages: ReadonlyArray<Package>;
 
   public constructor(app: App, config: ShipmentConfig) {
-    super(app, config, false);
-    this.trackingNumber = assert.string.nonWhitespace(config.trackingNumber, "tracking number");
-    this.identifiers = assert.array.ofIdentifiers(config.identifiers, "shipment identifiers", []);
+    super(app, config, true);
     this.packages = assert.array.nonEmpty(config.packages, "packages")
       .map((parcel: PackageConfig) => new Package(app, parcel));
 
     // Prevent modifications after validation
     Object.freeze(this);
-    Object.freeze(this.identifiers);
-    Object.freeze(this.billing);
-    Object.freeze(this.packages);
   }
+}
+
+
+function shipmentIdentifierMixin() {
+  return class ShipmentIdentifierMixin {
+    /**
+     * The carrier tracking number
+     */
+    public readonly trackingNumber: string;
+
+    /**
+     * Alternative identifiers associated with this shipment
+     */
+    public readonly identifiers: ReadonlyArray<Identifier>;
+
+    public constructor(config: ShipmentIdentifierConfig, isMixin: boolean) {
+      assert.type.object(config, "shipment");
+      this.trackingNumber = assert.string.nonWhitespace(config.trackingNumber, "tracking number");
+      this.identifiers = assert.array.ofIdentifiers(config.identifiers, "shipment identifiers", []);
+
+      // Prevent modifications after validation
+      Object.freeze(this.identifiers);
+
+      // Don't freeze the base object yet if this is a mixin
+      isMixin || Object.freeze(this);
+    }
+  };
+}
+
+function newShipmentMixin(base: Constructor = Object) {
+  return class NewShipmentMixin extends base {
+    /**
+     * The delivery service to use
+     */
+    public readonly deliveryService: DeliveryService;
+
+    /**
+     * The ID of the requested delivery confirmation
+     */
+    public readonly deliveryConfirmation?: DeliveryConfirmation;
+
+    /**
+     * The sender's contact info and address
+     */
+    public readonly shipFrom: Address;
+
+    /**
+     * The recipient's contact info and address
+     */
+    public readonly shipTo: Address;
+
+    /**
+     * The return address. Defautls to the `shipFrom` address
+     */
+    public readonly returnTo: Address;
+
+    /**
+     * The date/time that the package is expected to ship.
+     * This is not guaranteed to be in the future.
+     */
+    public readonly shipDateTime: Date;
+
+    /**
+     * Indicates how a non-deliverable package should be handled
+     */
+    public readonly nonDeliveryAction: NonDeliveryAction;
+
+    /**
+     * Which party will be insuring the shipment
+     */
+    public readonly insuranceProvider: InsuranceProvider;
+
+    /**
+     * The total insured value of all packages in the shipment
+     */
+    public readonly totalInsuredValue: MonetaryValue;
+
+    /**
+     * The original (outgoing) shipment that this return shipment is for.
+     * This associates the two shipments, which is required by some carriers.
+     * This field is `undefined` if this is not a return shipment, or if no
+     * outbound shipment was specified.
+     */
+    public readonly outboundShipment?: ShipmentIdentifier;
+
+    /**
+     * Indicates whether this is a return shipment
+     */
+    public readonly isReturn: boolean;
+
+    /**
+     * Indicates whether the shipment cannot be processed automatically due to size, shape, weight, etc.
+     * and requires manual handling.
+     *
+     * This property is `true` if any package in the shipment is non-machineable.
+     */
+    public get isNonMachineable(): boolean {
+      return this.packages.some((pkg) => pkg.isNonMachineable);
+    }
+
+    /**
+     * Billing details.
+     */
+    public billing: {
+      /**
+       * Indicates who customs duties are billed to. Defaults to the sender
+       */
+      dutiesPaidBy: BilledParty;
+
+      /**
+       * Indicates who delivery charges are billed to. Defaults to the sender
+       */
+      deliveryPaidBy: BilledParty;
+
+      /**
+       * The account number of the third-party that is responsible for shipping costs
+       */
+      account?: string;
+
+      /**
+       * The postal code of the third-party that is responsible for shipping costs
+       */
+      postalCode?: string;
+
+      /**
+       * The country of the third-party that is responsible for shipping costs
+       */
+      country?: Country;
+    };
+
+    /**
+     * The list of packages in the shipment
+     */
+    public readonly packages: ReadonlyArray<NewPackage>;
+
+    public constructor(app: App, config: NewShipmentConfig, isMixin: boolean) {
+      super(config, isMixin);
+      assert.type.object(config, "shipment");
+      this.deliveryService = app._references.lookup(config.deliveryServiceID, DeliveryService, "delivery service");
+      this.deliveryConfirmation = app._references.get(config.deliveryConfirmationID, DeliveryConfirmation, "delivery confirmation");
+      this.shipFrom = new Address(config.shipFrom);
+      this.shipTo = new Address(config.shipTo);
+      this.returnTo = config.returnTo ? new Address(config.returnTo) : new Address(config.shipFrom);
+      this.shipDateTime = assert.type.date(config.shipDateTime, "shipment date/time");
+      this.nonDeliveryAction = assert.string.enum(config.nonDeliveryAction, NonDeliveryAction, "non-delivery action");
+      this.insuranceProvider = assert.string.enum(
+          config.insuranceProvider, InsuranceProvider, "insurance provider", InsuranceProvider.Carrier);
+      this.outboundShipment = config.outboundShipment && new ShipmentIdentifier(config.outboundShipment);
+      this.isReturn = assert.type.boolean(config.isReturn, "isReturn flag", false);
+
+      // If there is no billing info, then the sender is billed by default.
+      // If billing a third-party, then account, postalCode, and country are required.
+      let billing = assert.type.object(config.billing, "billing info", {});
+      let isThirdParty =
+        (billing.dutiesPaidBy === BilledParty.ThirdParty) || (billing.deliveryPaidBy === BilledParty.ThirdParty);
+
+      this.billing = {
+        dutiesPaidBy: assert.string.enum(billing.dutiesPaidBy, BilledParty, "dutiesPaidBy", BilledParty.Sender),
+        deliveryPaidBy: assert.string.enum(billing.deliveryPaidBy, BilledParty, "deliveryPaidBy", BilledParty.Sender),
+        account: billing.account || isThirdParty
+          ? assert.string.nonWhitespace(billing.account, "billing account") : undefined,
+        postalCode: billing.postalCode || isThirdParty
+          ? assert.string.nonWhitespace(billing.postalCode, "billing postal code") : undefined,
+        country: billing.country || isThirdParty
+          ? assert.string.enum(billing.country, Country, "billing country") : undefined,
+      };
+
+      this.packages = assert.array.nonEmpty(config.packages, "packages")
+        .map((parcel: PackageConfig) => new NewPackage(app, parcel));
+
+      // NOTE: This must be calculated AFTER setting the packages property
+      this.totalInsuredValue = calculateTotalInsuranceAmount(this.packages);
+
+      // Prevent modifications after validation
+      Object.freeze(this.billing);
+      Object.freeze(this.packages);
+
+      // Don't freeze the base object yet if this is a mixin
+      isMixin || Object.freeze(this);
+    }
+  };
 }
 
 /**

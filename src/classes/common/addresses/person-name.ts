41a873d6db068b579539e1b0aec50b412acffc57
Randall Krauskopf <randall.krauskopf@gmail.com>
2020-05-07 12:46:49 -0500

    remove first/last properties to account for more international names

--- src/classes/common/addresses/person-name.ts
+++ src/classes/common/addresses/person-name.ts
@@ -20,49 +20,46 @@ export class PersonName {
 
   //#region Public Fields
 
-  public readonly first: string;
-  public readonly last: string;
+  public readonly title: string;
+  public readonly given: string;
+  public readonly middle: string;
+  public readonly family: string;
+  public readonly suffix: string;
 
   //#endregion
 
   public constructor(pojo: string | PersonNamePOJO) {
     if (typeof pojo === "string") {
-      pojo = PersonName.parse(pojo);
+      this.given = pojo;
+      this.title = "";
+      this.middle = "";
+      this.family = "";
+      this.suffix = "";
     }
 
-    this.first = pojo.first;
-    this.last = pojo.last;
+    else {
+      this.title = pojo.title || "";
+      this.given = pojo.given || "";
+      this.middle = pojo.middle || "";
+      this.family = pojo.family || "";
+      this.suffix = pojo.suffix || "";
+    }
 
     // Make this object immutable
     hideAndFreeze(this);
   }
 
   /**
-   * Parses a full name string
-   */
-  public static parse(fullName: string): PersonNamePOJO {
-    let separator = fullName.indexOf(" ");
-    if (separator >= 0) {
-      let first = fullName.slice(0, separator);
-      let last = fullName.slice(separator + 1);
-      return { first, last };
-    }
-    else {
-      return { first: fullName, last: "" };
-    }
-  }
-
-  /**
    * Returns the full name
    */
-  public toString(): string {
-    if (this.first && this.last) {
-      return `${this.first} ${this.last}`;
-    }
-    else {
-      return this.first || this.last;
-    }
-  }
+  // public toString(): string {
+  //   if (this.first && this.last) {
+  //     return `${this.first} ${this.last}`;
+  //   }
+  //   else {
+  //     return this.first || this.last;
+  //   }
+  // }
 }
 
 // Prevent modifications to the class

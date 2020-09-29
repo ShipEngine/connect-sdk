d768f522807e791b45f44ca08a4195abd8084b56
Mandy Hubbard <DevMandy@yahoo.com>
2020-09-29 11:55:42 -0500

    Remove useSandbox

--- src/public/common/transaction.ts
+++ src/public/common/transaction.ts
@@ -12,15 +12,6 @@ export interface Transaction<T extends object = object> {
   readonly id: UUID;
 
   /**
-   * Indicates whether the operation should use the carrier's sandbox/development API rather than
-   * the normal/production API.
-   *
-   * If `useSandbox` is `true`, then the operation MUST NOT incur any actual costs or affect
-   * production data.
-   */
-  readonly useSandbox: boolean;
-
-  /**
    * Arbitrary session data. Must be JSON serializable. Any method may update the session data,
    * such as renewing a session token or updating a timestamp.
    */

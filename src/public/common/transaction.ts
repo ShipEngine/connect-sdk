40f55170b3a9a1d6fd9d4de3ccc10f352d5c6eb2
Pierce Harmon <pierceh.dev@gmail.com>
2020-09-30 16:35:21 -0500

    W.I.P.

--- src/public/common/transaction.ts
+++ src/public/common/transaction.ts
@@ -34,6 +34,26 @@ export type OAuthSession<T extends object = object> = T & {
  * The ShpEngine Connect passes this object to every method call. It provides information about the
  * transaction being performed, including authentication, metadata, etc.
  */
+export interface OAuthTransaction<T extends object = object> {
+  /**
+   * Uniquely identifies the current transaction. If the transaction is retried, then this ID will
+   * remain the same. You can use this to detect/prevent duplicate operations.
+   */
+  readonly id: UUID;
+
+  /**
+   * Arbitrary session data that may include Auth data when set by a data driven auth processes.
+   * Must be JSON serializable. Any method may update the session data,
+   * such as renewing a session token or updating a timestamp.
+   */
+  session: OAuthSession<T>;
+}
+
+
+/**
+ * The ShpEngine Connect passes this object to every method call. It provides information about the
+ * transaction being performed, including authentication, metadata, etc.
+ */
 export interface Transaction<T extends object = object> {
   /**
    * Uniquely identifies the current transaction. If the transaction is retried, then this ID will
@@ -46,5 +66,5 @@ export interface Transaction<T extends object = object> {
    * Must be JSON serializable. Any method may update the session data,
    * such as renewing a session token or updating a timestamp.
    */
-  session: T | OAuthSession<T>;
+  session: T;
 }

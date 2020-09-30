8ec63d6b109743eb39f1445256a576d5802656af
rkrauskopf <randall.krauskopf@gmail.com>
2020-09-30 11:08:41 -0500

    revert some of the validation changes

--- src/internal/common/addresses/contact-info.ts
+++ src/internal/common/addresses/contact-info.ts
@@ -4,15 +4,12 @@ import { Joi } from "../validation";
 import { PersonName } from "./person-name";
 
 export class ContactInfoBase implements IContactInfo {
-  public readonly name?: PersonName;
-  public readonly email?: string;
-  public readonly phoneNumber?: string;
+  public readonly name: PersonName;
+  public readonly email: string;
+  public readonly phoneNumber: string;
 
   public constructor(pojo: ContactInfoPOJO) {
-    if (pojo.name) {
-      this.name = new PersonName(pojo.name);
-    }
-
+    this.name = new PersonName(pojo.name);
     this.email = pojo.email || "";
     this.phoneNumber = pojo.phoneNumber || "";
   }
@@ -22,9 +19,9 @@ export class ContactInfo extends ContactInfoBase {
   public static readonly [_internal] = {
     label: "contact info",
     schema: Joi.object({
-      name: PersonName[_internal].schema.optional(),
-      email: Joi.string().email().allow("").optional(),
-      phoneNumber: Joi.string().allow("").max(30).optional()
+      name: PersonName[_internal].schema.required(),
+      email: Joi.string().email().allow(""),
+      phoneNumber: Joi.string().trim().singleLine().allow("").max(30)
     }),
   };
 

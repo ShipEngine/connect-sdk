38a296a5f59f3590c23869e8b0c645baa0c0b5a1
Pierce Harmon <pierceh.dev@gmail.com>
2020-10-01 10:45:45 -0500

    W.I.P.

--- src/public/common/transaction.ts
+++ src/public/common/transaction.ts
@@ -1,13 +1,16 @@
 import type { UUID } from "./types";
 
 /**
- * Session data that includes auth data set by a data driven auth processes.
+ * Session data that may include auth data set by a data driven auth processes.
  * May include arbitrary data set by the developer.
  * Must be JSON serializable. Any method may update the session data,
  * such as renewing a session token or updating a timestamp.
  */
-export type OAuthSession<T extends object = object> = T & {
-  readonly auth: {
+export type Session<T extends object = object> = T & {
+  /**
+  * Auth object set by a data driven auth processes.
+  */
+  readonly auth?: {
     /**
      * Populated by the data driven auth processes for basic auth integrations.
      */
@@ -34,26 +37,6 @@ export type OAuthSession<T extends object = object> = T & {
  * The ShpEngine Connect passes this object to every method call. It provides information about the
  * transaction being performed, including authentication, metadata, etc.
  */
-export interface OAuthTransaction<T extends object = object> {
-  /**
-   * Uniquely identifies the current transaction. If the transaction is retried, then this ID will
-   * remain the same. You can use this to detect/prevent duplicate operations.
-   */
-  readonly id: UUID;
-
-  /**
-   * Arbitrary session data that may include Auth data when set by a data driven auth processes.
-   * Must be JSON serializable. Any method may update the session data,
-   * such as renewing a session token or updating a timestamp.
-   */
-  session: OAuthSession<T>;
-}
-
-
-/**
- * The ShpEngine Connect passes this object to every method call. It provides information about the
- * transaction being performed, including authentication, metadata, etc.
- */
 export interface Transaction<T extends object = object> {
   /**
    * Uniquely identifies the current transaction. If the transaction is retried, then this ID will
@@ -62,8 +45,10 @@ export interface Transaction<T extends object = object> {
   readonly id: UUID;
 
   /**
-   * Arbitrary session data. Must be JSON serializable. Any method may update the session data,
+   * Session data that may include auth data set by a data driven auth processes.
+   * May include arbitrary data set by the developer.
+   * Must be JSON serializable. Any method may update the session data,
    * such as renewing a session token or updating a timestamp.
    */
-  session: T;
+  session: Session<T>;
 }

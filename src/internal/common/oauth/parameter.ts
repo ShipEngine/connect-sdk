bb0385fbdff541394876016461d1a86e9fb37346
rkrauskopf <randall.krauskopf@gmail.com>
2020-12-01 15:56:37 -0600

    passing tests

--- src/internal/common/oauth/parameter.ts
+++ src/internal/common/oauth/parameter.ts
@@ -8,18 +8,18 @@ export class OAuthParameter {
     schema: Joi.object({
       name: Joi.string().required(),
       value: Joi.string().required(),
-      encoding: Joi.string().valid("base64", "basic-auth").optional(),
+      encoding: Joi.string().valid("base64", "basic-auth", "").optional(),
     })
   };
 
   public readonly name: string;
   public readonly value: string;
-  public readonly encoding?: string;
+  public readonly encoding: string;
 
   public constructor(pojo: OAuthParameterDefinition) {
     this.name = pojo.name;
     this.value = pojo.value;
-    this.encoding = pojo.encoding;
+    this.encoding = pojo.encoding ? pojo.encoding : "";
 
     // Make this object immutable
     hideAndFreeze(this);

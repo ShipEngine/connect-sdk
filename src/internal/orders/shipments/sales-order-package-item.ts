25ce2ea4703a7b67bab3c43e39c31bb83507ce4a
Justin Robertson <justin.robertson@shipstation.com>
2020-12-04 15:40:50 -0600

    Added Currency and Notes to SalesOrderPackage

--- src/internal/orders/shipments/sales-order-package-item.ts
+++ src/internal/orders/shipments/sales-order-package-item.ts
@@ -1,5 +1,5 @@
 import { ProductIdentifierPOJO, QuantityPOJO, SalesOrderItemIdentifierPOJO, SalesOrderPackageItem as ISalesOrderPackageItem } from "../../../public";
-import { hideAndFreeze, Joi, Quantity, _internal } from "../../common";
+import { hideAndFreeze, Joi, Note, Quantity, _internal } from "../../common";
 import { SalesOrderItemIdentifier } from "../../orders/sales-order-item-identifier";
 import { ProductIdentifier } from "../../products";
 
@@ -7,6 +7,8 @@ export interface SalesOrderPackageItemPOJO {
   salesOrderItem: SalesOrderItemIdentifierPOJO;
   product?: ProductIdentifierPOJO;
   quantity: QuantityPOJO;
+  currency?: string;
+  notes?: Note[];
 }
 
 
@@ -17,18 +19,23 @@ export class SalesOrderPackageItem implements ISalesOrderPackageItem {
       salesOrderItem: SalesOrderItemIdentifier[_internal].schema.unknown(true),
       product: ProductIdentifier[_internal].schema.unknown(true),
       quantity: Quantity[_internal].schema.required(),
+      currency: Joi.string().optional().allow(""),
+      notes: Joi.array().optional().items(Note[_internal].schema),
     }),
   };
 
   public readonly salesOrderItem?: SalesOrderItemIdentifier;
   public readonly product?: ProductIdentifier;
   public readonly quantity: Quantity;
+  public readonly currency?: string;
+  public readonly notes?: Note[];
 
   public constructor(pojo: SalesOrderPackageItemPOJO) {
     this.salesOrderItem = new SalesOrderItemIdentifier(pojo.salesOrderItem);
     this.product = pojo.product && new ProductIdentifier(pojo.product);
     this.quantity = new Quantity(pojo.quantity);
-
+    this.currency = pojo.currency;
+    this.notes = pojo.notes;
     // Make this object immutable
     hideAndFreeze(this);
   }

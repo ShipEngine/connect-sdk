d4c583109ee8fce8403ad0c44463305f53a2008d
Josh Eversmann <josh.eversmann@shipstation.com>
2021-01-08 13:05:15 -0600

    formatting

--- src/internal/orders/requested-fulfillment.ts
+++ src/internal/orders/requested-fulfillment.ts
@@ -6,24 +6,24 @@ import { SalesOrderItem } from "./sales-order-item";
 import { ShippingPreferences } from "./shipping-preferences";
 
 export class RequestedFulfillment {
-    public static readonly [_internal] = {
-      label: "requested fulfillment",
-      schema: Joi.object({
-        items: Joi.array().min(1).items(SalesOrderItem[_internal].schema.optional()).required(),
-        shippingPreferences: ShippingPreferences[_internal].schema,
-        shipTo: AddressWithContactInfo[_internal].schema.required(),
-      })
-    };
+  public static readonly [_internal] = {
+    label: "requested fulfillment",
+    schema: Joi.object({
+      items: Joi.array().min(1).items(SalesOrderItem[_internal].schema.optional()).required(),
+      shippingPreferences: ShippingPreferences[_internal].schema,
+      shipTo: AddressWithContactInfo[_internal].schema.required(),
+    })
+  };
 
-    public readonly items: SalesOrderItem[];
-    public readonly shippingPreferences: ShippingPreferences;
-    public readonly shipTo: AddressWithContactInfo;
-    public readonly extensions?: RequestedFulfillmentExtensions;
+  public readonly items: SalesOrderItem[];
+  public readonly shippingPreferences: ShippingPreferences;
+  public readonly shipTo: AddressWithContactInfo;
+  public readonly extensions?: RequestedFulfillmentExtensions;
 
-    public constructor(pojo: RequestedFulfillmentPOJO) {
-        this.items = pojo.items.map((item) => new SalesOrderItem(item)),
-        this.shippingPreferences = new ShippingPreferences(pojo.shippingPreferences || {}),
-        this.shipTo = new AddressWithContactInfo(pojo.shipTo),
-        this.extensions = pojo.extensions
-    }
+  public constructor(pojo: RequestedFulfillmentPOJO) {
+    this.items = pojo.items.map((item) => new SalesOrderItem(item));
+    this.shippingPreferences = new ShippingPreferences(pojo.shippingPreferences || {});
+    this.shipTo = new AddressWithContactInfo(pojo.shipTo);
+    this.extensions = pojo.extensions;
   }
+}
